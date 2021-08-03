9b4670c95978e994af613279db71c9b3077fd6a3
stefan-kanazir <kanazir.stefan@gmail.com>
2021-08-03 17:52:51 +0200

    Navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        "bookmark-purple": "#5267DF",
+        "bookmark-red": "#FA5959",
+        "bookmark-blue": "#242A45",
+        "bookmark-grey": "#9194A2",
+        "bookmark-white": "#f7f7f7",
+      },
     },
     fontFamily: {
       Poppins: ["Poppins, sans-serif"],
