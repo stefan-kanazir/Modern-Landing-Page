df0209a6390c9a03220f16f2ac4f894e1e7be85e
stefan-kanazir <kanazir.stefan@gmail.com>
2021-08-03 17:21:10 +0200

    Test

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,7 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        "bookmark-purple": "#5267DF",
-        "bookmark-red": "#FA5959",
-        "bookmark-blue": "#242A45",
-        "bookmark-grey": "#9194A2",
-        "bookmark-white": "#f7f7f7",
-      },
-    },
+    extend: {},
     fontFamily: {
       Poppins: ["Poppins, sans-serif"],
     },
